6502a24f2bdaebcae3ad6f795306c313a2153f9a
Danilo Čoda Vukomanović <dvukomanovic@gmail.com>
2016-11-02 02:24:28 +0100

    camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,23 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using System;
 
-public class CameraController : MonoBehaviour {
-
-    public CameraFollowState follow = new CameraFollowState();
-    
+public class CameraController : MonoBehaviour
+{
+    public CameraFollowState followState = new CameraFollowState(1, true);
+    public bool follow;
     protected List<State> _stateStack = new List<State>();
+
+    void Start()
+    {
+        if (this.follow)
+        {
+            AddState(this.followState);
+        }
+    }
 
     public void AddState(State state)
     {
         if (!this._stateStack.Contains(state))
+        {
             this._stateStack.Add(state);
+            state.EnterState();
+        }
+    }
+
+    public void StackState(State state)
+    {
+        this._stateStack.Add(state);
+        state.EnterState();
+    }
+
+    public void RemoveState(State state)
+    {
+        this._stateStack.Remove(state);
+        state.ExitState();
+    }
+
+    public bool HasState(State state)
+    {
+        return this._stateStack.Contains(state);
     }
 }
 
 public interface IState
 {
-    void Initialize();
+    void Initialize(float duration, bool loop);
     void EnterState();
     bool Update();
     void ExitState();
@@ -31,7 +58,7 @@ public abstract class State : IState
     protected bool _isInitialized;
     public bool loop;
 
-    public abstract void Initialize();
+    public abstract void Initialize(float duration, bool loop);
     public abstract void EnterState();
     public abstract void ExitState();
     public abstract bool Update();
@@ -40,20 +67,21 @@ public abstract class State : IState
 [System.Serializable]
 public class CameraFollowState : State
 {
-    public CameraFollowState()
+    public CameraFollowState(float duration, bool loop)
     {
-        Initialize();
+        Initialize(duration, loop);
     }
 
-    public override void Initialize()
+    public override void Initialize(float duration, bool loop)
     {
-      //nuthin'
+        this.duration = duration;
+        this.loop = loop;
     }
 
     public override void EnterState()
     {
         if (!this._isInitialized)
-            Initialize();
+            Initialize(this.duration, this.loop);
 
         this._timer = this.duration;
     }
