1320e568bd72c52279a8b48745bc2959384af532
Danilo Čoda Vukomanović <dvukomanovic@gmail.com>
2016-11-05 17:45:10 +0100

    more on follow

--- Assets/Game/Scripts/CameraController.cs
+++ Assets/Game/Scripts/CameraController.cs
@@ -36,6 +36,13 @@
 			this.follow = true;
             AddState(this.UpdateBackground);
 		}
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(this.followBounds.center,this.followBounds.extents*2);
+        }
+
         #endregion Mono
 
         #region Follow
@@ -44,7 +51,7 @@
         public float followSpeed = 1;
         public Vector3 followOffset;
         private bool _follow = false;
-
+        public Bounds followBounds;
         public bool follow
         {
             get { return this._follow; }
@@ -73,7 +80,7 @@
                 return;
             }
 
-			AddState (this.Follow);
+            AddState (this.Follow);
 		}
 
 		void Follow ()
@@ -81,6 +88,9 @@
             Vector3 newPosition = Vector3.MoveTowards(this._selfTransform.position, this.target.position + this.followOffset, this.followSpeed * Time.smoothDeltaTime);
             newPosition[2] = this._selfTransform.position[2];
             this._selfTransform.position = newPosition;
+            if (!this.followBounds.Contains(this.transform.position)){
+                ExitFollowState();
+            }
 		}
 
         void FollowRB()
@@ -88,11 +98,17 @@
            // float _speed = Mathf.Max(this.followSpeed,Mathf.Min (this._targetRB.velocity.sqrMagnitude * 0.01f,1000f));
 
             float _speed = Mathf.Clamp(this._targetRB.velocity.sqrMagnitude * 0.01f, this.followSpeed, 1000f);
+            Vector3 newPosition = this._selfTransform.position;
+            newPosition[0] = Mathf.MoveTowards(newPosition[0],this.target.position[0],this.followSpeed*Time.smoothDeltaTime);
+            newPosition[1] = Mathf.MoveTowards(newPosition[1], this.target.position[1], _speed * Time.smoothDeltaTime);
 
-            Vector3 newPosition = Vector3.MoveTowards(this._selfTransform.position, this.target.position + this.followOffset, _speed * Time.smoothDeltaTime);
-            newPosition[2] = this._selfTransform.position[2];
+            //Vector3 newPosition = Vector3.MoveTowards(this._selfTransform.position, this.target.position + this.followOffset, _speed * Time.smoothDeltaTime);
+            //newPosition[2] = this._selfTransform.position[2];
             this._selfTransform.position = newPosition;
-
+            
+            if (!this.followBounds.Contains(this.transform.position)){
+                ExitFollowState();
+            }
         }
 
         void UpdateBackground()
