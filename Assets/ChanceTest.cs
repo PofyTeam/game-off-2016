c8a95909daf2896576f0cc3ba87b65441680f536
Danilo Vukomanovic <dvukomanovic@gmail.com>
2016-11-18 16:58:22 +0100

    chance test

---
+++ Assets/ChanceTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using PofyTools.Distribution;
+
+public class ChanceTest : MonoBehaviour
+{
+	[Range (0f, 1f)]public float chance;
+	private Chance _chance;
+	//public List<bool> _result;
+	// Use this for initialization
+	void Start ()
+	{
+		this._chance = new Chance (chance, true);
+		ToString ();
+		this._chance.autoDeckSize = false;
+		this._chance.chance = 0.123f;
+		ToString ();
+
+		this._chance.autoDeckSize = true;
+		this._chance.chance = 0.75f;
+		ToString ();
+
+
+	}
+
+	void ToString ()
+	{
+		Debug.Log (_chance.Count);
+		for (int i = 0; i < 1000; ++i) {
+			//this._result.Add (chance.Value);
+			Debug.Log (_chance.Value);
+		}
+	}
+}
